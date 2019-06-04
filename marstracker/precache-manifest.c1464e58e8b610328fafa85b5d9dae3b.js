4bee3ae27a849186a0791c5088c2f2d141e59731
Moorthi <pvmoorthi_jobs@yahoo.com>
2019-06-03 18:53:16 -0700

    2nd

---
+++ marstracker/precache-manifest.c1464e58e8b610328fafa85b5d9dae3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c37c7a43f9ad5eb16fc52327741662d4",
+    "url": "/static/media/car-marker.c37c7a43.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b2332f74eef7567ae427",
+    "url": "/static/js/main.b2332f74.chunk.js"
+  },
+  {
+    "revision": "9f6c1f8fe5bf9e19b1ed",
+    "url": "/static/js/1.9f6c1f8f.chunk.js"
+  },
+  {
+    "revision": "b2332f74eef7567ae427",
+    "url": "/static/css/main.51cef88d.chunk.css"
+  },
+  {
+    "revision": "9f6c1f8fe5bf9e19b1ed",
+    "url": "/static/css/1.ea1aa506.chunk.css"
+  },
+  {
+    "revision": "5e96d05e3670173690a329cf5b520c48",
+    "url": "/index.html"
+  }
+];
--- marstracker/service-worker.js
+++ marstracker/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tests/precache-manifest.8d13b82b23ce74ab572492fe0c408368.js"
+  "/precache-manifest.c1464e58e8b610328fafa85b5d9dae3b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/tests/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
