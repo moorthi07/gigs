d902beddc0e9bb35a5b87891194b514ad5c123f5
PV <pvmoorthi_jobs@yahoo.com>
2020-08-03 21:47:41 -0700

    Add files via upload
    fixed responsive chart issue.

--- marstracker/service-worker.js
+++ marstracker/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gigs/marstracker/precache-manifest.f653e80dc1a5d496e3c0a92a54975f54.js"
+  "/tests/precache-manifest.40ab7228f8d08b44bfe14fb9a2cbd104.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/gigs/marstracker/index.html", {
+workbox.routing.registerNavigationRoute("/tests/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
