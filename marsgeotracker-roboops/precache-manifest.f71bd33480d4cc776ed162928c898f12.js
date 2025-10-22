821b7cdaa9377d0cdb6992a0249b89017df219b0
moorthi07 <pvmoorthi_jobs@yahoo.com>
2025-10-22 04:18:48 -0700

    mgtrkr25 roboops added

---
+++ marsgeotracker-roboops/precache-manifest.f71bd33480d4cc776ed162928c898f12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6434f2279d545e7a66bc",
+    "url": "/static/css/main.02d84401.chunk.css"
+  },
+  {
+    "revision": "6434f2279d545e7a66bc",
+    "url": "/static/js/main.6434f227.chunk.js"
+  },
+  {
+    "revision": "70f6a75763123d1f8b6e",
+    "url": "/static/css/1.b1b90032.chunk.css"
+  },
+  {
+    "revision": "70f6a75763123d1f8b6e",
+    "url": "/static/js/1.70f6a757.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c37c7a43f9ad5eb16fc52327741662d4",
+    "url": "/static/media/car-marker.c37c7a43.png"
+  },
+  {
+    "revision": "9645454bb86c41c5d50ac34599a14b75",
+    "url": "/index.html"
+  }
+];
